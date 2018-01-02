f8978ae0f0dfc2284f282b98a38ce6d673822640
yuanyuanjun <yuanyuanjun@ascs.tech>
2018-01-02 14:11:08 +0800

    feat:按照官网修改前

--- pub/js/media.js
+++ pub/js/media.js
@@ -1,21 +1,28 @@
+var isAddIceCandidate = false;
 var chatTool = {};
 var video = document.querySelector('video');
+var othersCD = [];
 var iceServer = {
     "iceServers": [
+        // {"urls": "stun:stun.l.google.com:19302"},
         // {
-        //     "urls": "stun:stun.l.google.com:19302"
-        // }, {
-        //     "urls": "turn:numb.viagenie.ca",
-        //     "username": "webrtc@live.com",
-        //     "credential": "muazkh"
-        // }
+        //     "urls": "stun:stunserver.org"
+        // },
+        // {
+        //    "urls": "stun:stun.ideasip.com"
+        //},
         {
-            "urls": "stun:stunserver.org"
-        }, {
-            "urls":"turn:106.15.225.7",
-            credential:"username",
-            username:"userpwd",
-        }
+            "urls":"stun:106.15.225.7:3478",
+            "username":"user",
+            "credential":"password",
+        },
+        {
+            "urls":"turn:106.15.225.7:3478",
+            "username":"user",
+            "credential":"password",
+        },
+        {'urls': 'stun:stun.services.mozilla.com'},
+        {'urls': 'stun:stun.l.google.com:19302'}
     ]
     
 };
@@ -23,19 +30,40 @@ var PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || 
 var pc = new PeerConnection(iceServer);
 chatTool.socket = io.connect();
 pc.ontrack = function(evt) {
+    console.log("ontrack",evt.streams);
     video.srcObject = evt.streams[0];
 };
-// video.onloadedmetadata = function(e) {
-//    // Do something with the video here.
-// };
+//发送ICE候选到其他客户端
+pc.onicecandidate = function(event){
+    // console.log("onicecandidate",event.candidate);
+    if (!event || !event.candidate) {
+        console.log("no candidate")
+    }else{
+        chatTool.socket.emit( "sendCd",{"man":linkUserName,"candidate": event.candidate} );
+        console.log("candidate sended")
+    }
+};
+//接收ICE候选
+chatTool.socket.on( "getCd",function (val) {
+    // if(isAddIceCandidate){
+    othersCD.push(val.candidate);
+    pc.addIceCandidate(new RTCIceCandidate(val.candidate))
+        .then(function () {
+            console.log("addIceCandidate");
+        })
+        .catch(function (err) {
+            console.log(err,"addIceCandidate failed")
+        })
+    // }else {console.log("not ready!")}
+} );
+
 //主动呼叫，打开摄像头
 function mediaStart(userName,linkUserName) {
     var userMedia = navigator.mediaDevices.getUserMedia({ audio: false, video: true });
     userMedia
         .then(start)
         .catch(error=>{
-            test.innerText = JSON.stringify(error);
-            console.error("mediaStart,getUserMedia"+error)
+            console.error("mediaStart,getUserMedia#"+error)
         });
     function start(mediaStream) {
         video.srcObject = mediaStream;
@@ -43,35 +71,52 @@ function mediaStart(userName,linkUserName) {
         //创建并发送视频请求
         pc.createOffer(function(offer) {
             pc.setLocalDescription(new RTCSessionDescription(offer), function() {
+                console.log("call,setLocalDescription");
                 chatTool.socket.emit("linkTo",{user:userName,man:linkUserName,offer:offer});
                 // send the offer to a server to be forwarded to the friend you're calling.
-            }, error=>{console.error("mediaStart,setLocalDescription"+error)});
-        }, error=>{console.error("mediaStart,createOffer"+error)});
+            }, error=>{console.error("mediaStart,setLocalDescription#"+error)});
+        }, error=>{console.error("mediaStart,createOffer#"+error)});
     }
 }
 //被呼叫，同意连接
 function wasCalled(val){
     var offer = val.offer;
     var userMedia = navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+    linkUserName = val.man;
     userMedia
         .then(answer)
         .catch(error=>{
-            console.error("wasCalled,getUserMedia"+error)
+            console.error("wasCalled,getUserMedia#"+error)
         });
     function answer(stream) {
-        // pc.ontrack({stream: stream});
         pc.addStream(stream);
         pc.setRemoteDescription(new RTCSessionDescription(offer), function() {
+            console.log("wascall,setRemoteDescription");
+            isAddIceCandidate = true;
+            // setTimeout(addCD,3000)
             pc.createAnswer(function(answer) {
                 pc.setLocalDescription(new RTCSessionDescription(answer), function() {
+                    console.log("wascall,setLocalDescription");
                     test.innerText = val.man;
                     chatTool.socket.emit("linkAnswer",{man:val.man,answer:answer});
-                }, error=>{console.error("wasCalled,setLocalDescription"+error)});
-            }, error=>{console.error("wasCalled,setLocalDescription"+error)});
-        }, error=>{console.error("wasCalled,setRemoteDescription"+error)});
+                }, error=>{console.error("wasCalled,setLocalDescription#"+error)});
+            }, error=>{console.error("wasCalled,setLocalDescription#"+error)});
+        }, error=>{console.error("wasCalled,setRemoteDescription#"+error)});
     }
 }
-
+//添加CD
+function addCD() {
+    console.log("addCD",othersCD);
+    othersCD.forEach(item=>{
+        pc.addIceCandidate(new RTCIceCandidate(item))
+            .then(function () {
+                console.log("addIceCandidate");
+            })
+            .catch(function (err) {
+                console.log(err,"addIceCandidate failed")
+            })
+    })
+}
 
 var test = document.querySelector("#test");
 var user = document.querySelector("#user");
@@ -96,6 +141,13 @@ chatTool.socket.on("linkOk",function (val) {
     var answer = val.offer;
     pc.setRemoteDescription(
         new RTCSessionDescription(answer),
-        function() {},
+        function() {
+            console.log("call,setRemoteDescription")
+            isAddIceCandidate = true;
+            // setTimeout(addCD,3000)
+        },
+        function (err) {
+            console.log(err,"linkOk,setRemoteDescription")
+        }
     );
 });
